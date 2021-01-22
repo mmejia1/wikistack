702d7f86862e0865fc7589b9186a7cc94f947321
Kristen Studioso <studiosok@gmail.com>
2021-01-22 15:57:33 -0500

    trying this out

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const morgan = require('morgan');
-const app = express();
-const bodyParser = require('body-parser');
-const layOut = require('./views/layout');
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const bodyParser = require('body-parser')
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'))
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}))
 
 app.get('/', (req, res) => {
-  res.send(layOut('hello world'));
-});
+  res.send('hello world')
+})
 
-const port = '1337';
+const port = '1337'
 
 app.listen(port, () => {
-  console.log('listening on port ', port);
-});
+  console.log('listening on port ', port)
+})
