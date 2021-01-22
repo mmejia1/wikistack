cc6f009780078622801574e1bd48c5e2fe57943e
Marjorie Mejia <mm035359@gmail.com>
2021-01-22 15:45:23 -0500

    setup

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+const layOut = require('./views/layout');
 
-app.use(morgan('dev'))
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('hello world')
-})
+  res.send(layOut('hello world'));
+});
 
-const port = '1337'
+const port = '1337';
 
 app.listen(port, () => {
-  console.log('listening on port ', port)
-})
+  console.log('listening on port ', port);
+});
