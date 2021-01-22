6537b6559bdf383462b7d81f5c05aea529d4b8c2
Marjorie Mejia <mm035359@gmail.com>
2021-01-22 16:59:38 -0500

    sequelize models

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const express = require('express')
-const morgan = require('morgan')
-const app = express()
-const bodyParser = require('body-parser')
-const layOut = require('./views/layout')
-const { db } = require('./models')
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const bodyParser = require('body-parser');
+const layOut = require('./views/layout');
+const { db } = require('./models');
+const Page = require('./models');
 
-app.use(morgan('dev'))
-app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database!')
-  })
+db.authenticate().then(() => {
+  console.log('connected to the database!');
+});
 
 app.get('/', (req, res) => {
   res.send(layOut('hello world'));
-})
+});
 
-const port = '1337'
+async function syncModels() {
+  try {
+    await Page.sync();
+    await User.sync();
+  } catch (err) {
+    console.log(err);
+  }
+}
+syncModels();
+const port = '1337';
 
 app.listen(port, () => {
-  console.log('listening on port ', port)
-})
+  console.log('listening on port ', port);
+});
--- models/index.js
+++ models/index.js
@@ -1,6 +1,38 @@
-const Sequelize = require('sequelize')
-const db = new Sequelize('postgres://localhost:5432/wikistack')
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
 
+const Page = db.define('page', {
+  title: Sequelize.STRING,
+  slug: {
+    type: Sequelize.STRING,
+    validate: {
+      unique: true,
+      allowNull: false,
+      len: [3, 20],
+      isAlphanumeric: true,
+    },
+    content: Sequelize.TEXT,
+    status: Sequelize.BOOLEAN,
+  },
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    validate: {
+      isAlpha: true,
+      //notNull: true,
+    },
+  },
+  email: {
+    type: Sequelize.STRING,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
 module.exports = {
-  db
-}
+  db,
+  Page,
+  User,
+};
